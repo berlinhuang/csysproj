fdbd2736bf4c75de57495b2253f6ec04373ca11c
berlinhuang <berlinmail@qq.com>
2017-11-08 17:24:20 +0800

    firstly commit

--- IPC/fifo_read.c
+++ IPC/fifo_read.c
@@ -2,6 +2,24 @@
 // Created by root on 17-11-8.
 //
 
+/**
+
+当只写打开FIFO管道时，如果没有FIFO没有读端打开，则open写打开会阻塞。
+FIFO内核实现时可以支持双向通信。（pipe单向通信，因为父子进程共享同一个file 结构体）
+FIFO可以一个读端，多个写端；也可以一个写端，多个读端。
+
+
+
+命令行下建一个fifo : mkfifo myfifo
+也可以用api: int mkfifo(const char *pathname, mode_t mode)
+两个文件编译成fifo_w fifo_r
+调用写： ./fifo_w myfifo
+调用读： ./fifo_r myfifo
+
+ */
+
+
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
