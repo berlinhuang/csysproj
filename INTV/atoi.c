be061da34533a2c67b63e4b2f1c1043a9c88579e
berlinhuang <berlinmail@qq.com>
2017-11-20 09:24:34 +0800

    add atoi()

--- INTV/atoi.c
+++ INTV/atoi.c
@@ -6,7 +6,16 @@
 
 int main(void )
 {
+    char *s1="333640";
+    char *s2="-12345";
     char *s3="123.3113";
-    printf("integer=%d\n",my_atoi(s3));
+    char *s4="-8362865623872387698";
+    char *s5="+246653278";
+
+    printf("integer1=%d\n",my_atoi(s1));
+    printf("integer2=%d\n",my_atoi(s2));
+    printf("integer3=%d\n",my_atoi(s3));
+    printf("integer4=%d\n",my_atoi(s4));
+    printf("integer5=%d\n",my_atoi(s5));
     return 0;
 }
